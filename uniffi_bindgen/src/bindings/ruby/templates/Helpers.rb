9ad0c7755d1b277b775d1648f34e2e81d401e28e
heinrich5991 <heinrich5991@gmail.com>
2023-06-01 12:05:37 +0200

    Check for integer overflows in Ruby bindings
    The exception thrown is
    [`RangeError`](https://docs.ruby-lang.org/en/master/RangeError.html),
    like e.g. when doing `256.chr`.
    
    ```
    > 256.chr
    (irb):1:in `chr': 256 out of char range (RangeError)
            from (irb):1:in `<main>'
            from /usr/lib/ruby/gems/3.0.0/gems/irb-1.4.2/exe/irb:11:in `<top (required)>'
            from /usr/bin/irb:25:in `load'
            from /usr/bin/irb:25:in `<main>'
    ```

---
+++ uniffi_bindgen/src/bindings/ruby/templates/Helpers.rb
@@ -0,0 +1,7 @@
+def uniffi_in_range(i, type_name, min, max)
+    i = i.to_i
+    if not (min <= i and i < max)
+        raise RangeError.new "#{type_name} requires #{min} <= value < #{max}"
+    end
+    i
+end
