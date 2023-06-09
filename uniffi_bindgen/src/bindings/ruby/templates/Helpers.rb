f04fd52e1a85573dfa656b52bd2a29ba42aa0fad
Aliaksandr Rahalevich <alex.rahalevich@dr-bill.ca>
2023-06-09 16:53:53 -0700

    move "uniffi_in_range" into module

--- uniffi_bindgen/src/bindings/ruby/templates/Helpers.rb
+++ uniffi_bindgen/src/bindings/ruby/templates/Helpers.rb
@@ -1,4 +1,4 @@
-def uniffi_in_range(i, type_name, min, max)
+def self.uniffi_in_range(i, type_name, min, max)
     i = i.to_i
     if not (min <= i and i < max)
         raise RangeError.new "#{type_name} requires #{min} <= value < #{max}"
